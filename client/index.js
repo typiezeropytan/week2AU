96581ed51903f8a7c5efee6cff8a21e914a64754
Kacper Kuś <kacperkus@Kacpers-MacBook-Air.local>
2023-11-03 19:55:58 +0100

    Komunikat

--- client/index.js
+++ client/index.js
@@ -1,14 +1,23 @@
 const axios = require('axios');
 const niceList = require('../utils/niceList.json');
-const MerkleTree = require('../utils/MerkleTree');
+const MerkleTree = require('../utils/MerkleTree'); // Ensure you have a MerkleTree implementation
 
 const serverUrl = 'http://localhost:1225';
 
 async function main() {
-  // TODO: how do we prove to the server we're on the nice list? 
+  const nameToCheck = 'Your Name'; // Replace with the name you want to check
+  const leafNodes = niceList.names.map((name) => crypto.createHash('sha256').update(name).digest('hex'));
+  
+  // Create a Merkle tree
+  const merkleTree = new MerkleTree(leafNodes);
 
+  // Generate a Merkle proof for the name you want to check
+  const proof = merkleTree.getProof(nameToCheck);
+
+  // Send the POST request to the server with the name and proof
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
-    // TODO: add request body parameters here!
+    name: nameToCheck,
+    proof: proof,
   });
 
   console.log({ gift });
--- server/index.js
+++ server/index.js
@@ -1,25 +1,75 @@
 const express = require('express');
-const verifyProof = require('../utils/verifyProof');
+const crypto = require('crypto'); // Import the crypto module
 
 const port = 1225;
 
 const app = express();
 app.use(express.json());
 
-// TODO: hardcode a merkle root here representing the whole nice list
-// paste the hex string in here, without the 0x prefix
-const MERKLE_ROOT = '';
+// Parse the niceList JSON (assuming niceList.json is in the same directory as your script)
+const niceList = require('./utils/niceList.json');
 
-app.post('/gift', (req, res) => {
-  // grab the parameters from the front-end here
-  const body = req.body;
 
-  // TODO: prove that a name is in the list 
-  const isInTheList = false;
-  if(isInTheList) {
-    res.send("You got a toy robot!");
+// Create an array of leaf nodes (in this case, we'll use the names as leaf nodes)
+const leafNodes = niceList.names.map((name) => crypto.createHash('sha256').update(name).digest('hex'));
+
+// Function to build a Merkle Tree
+function buildMerkleTree(leafNodes) {
+  if (leafNodes.length === 0) {
+    return null;
   }
-  else {
+
+  if (leafNodes.length === 1) {
+    return leafNodes[0];
+  }
+
+  const nextLevel = [];
+  for (let i = 0; i < leafNodes.length; i += 2) {
+    const left = leafNodes[i];
+    const right = i + 1 < leafNodes.length ? leafNodes[i + 1] : '';
+    const combinedHash = crypto.createHash('sha256').update(left + right).digest('hex');
+    nextLevel.push(combinedHash);
+  }
+
+  return buildMerkleTree(nextLevel);
+}
+
+// Build the Merkle Tree
+const merkleRoot = buildMerkleTree(leafNodes);
+
+// TODO: Hardcode the calculated Merkle Root here
+// Paste the hex string in the MERKLE_ROOT variable, without the 0x prefix
+const MERKLE_ROOT = merkleRoot;
+
+// Function to verify a Merkle proof
+function verifyProof(name, root, leafNodes, proof) {
+  const leafHash = crypto.createHash('sha256').update(name).digest('hex');
+  let currentHash = leafHash;
+
+  for (const proofElement of proof) {
+    if (proofElement.position === 'left') {
+      currentHash = crypto.createHash('sha256').update(currentHash + proofElement.sibling).digest('hex');
+    } else {
+      currentHash = crypto.createHash('sha256').update(proofElement.sibling + currentHash).digest('hex');
+    }
+  }
+
+  return currentHash === root;
+}
+
+// You can now use this function in your /gift route
+app.post('/gift', (req, res) => {
+  // Grab the parameters from the front-end here
+  const body = req.body;
+  const name = body.name; // Assuming the name is in the request body
+  const proof = body.proof; // Assuming the proof is in the request body
+
+  // Prove that a name is in the list by checking the Merkle proof
+  const isInTheList = verifyProof(name, MERKLE_ROOT, leafNodes, proof);
+
+  if (isInTheList) {
+    res.send("You got a toy robot!");
+  } else {
     res.send("You are not on the list :(");
   }
 });
